5ce4afe886576fd10648988197acc41f2d5e410f
pancitoConQuesito <gonzalo.astorga.@gmail.com>
2021-08-31 09:59:28 -0400

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,40 @@
-const btn=document.getElementById("btn");
-btn.addEventListener("click", function(event){
-    var miDiv=document.getElementById("casi");
-    miDiv.classList.add("cubo");
-});
+"use strict";
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+var cajaALL_padre = document.getElementById("caja");
+//var contador=0;
+var fragmento = document.createDocumentFragment();
+for (var i = 0; i < 10; i++) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    div.className = "cajita";
+    var x = getRandomInt(400, 600);
+    var y = getRandomInt(500, 800);
+    img.src = "https://loremflickr.com/" + x + "/" + y + "/cat";
+    var div_mini = document.createElement("div");
+    var icono = document.createElement("span");
+    var numero = document.createElement("span");
+    icono.className = "material-icons";
+    numero.id = "fuente";
+    // icono.id=String(contador);//
+    // numero.className=String(contador);//
+    // contador++;//
+    icono.addEventListener("click", function (event) {
+        // var fuente = document.getElementById("fuente");
+        // var numeroActual = document.getElementsByClassName(String(event.target.id));
+        // for(var i=0;i<numeroActual.length;i++){
+        //     numeroActual[i].innerHTML=String(parseInt(numeroActual[i].textContent)+1);
+        // }
+        var numeroActual = event.target.parentNode.lastChild;
+        numeroActual.textContent = parseInt(numeroActual.textContent) + 1;
+        //event.target.parentNode.lastChild.innerHTML=parseInt(event.target.parentNode.lastChild.innerHTML)+1;
+    });
+    div_mini.appendChild(icono).textContent = "favorite";
+    div_mini.appendChild(numero).textContent = "1";
+    div.appendChild(img);
+    div.appendChild(div_mini);
+    //cajaALL_padre.appendChild(div);
+    fragmento.appendChild(div);
+}
+cajaALL_padre.appendChild(fragmento);
